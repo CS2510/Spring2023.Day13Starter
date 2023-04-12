8a13cf584dc75f7f7c4495d61b520f29fe1ffa93
bricksphd <bricks@unomaha.edu>
2023-04-12 11:30:55 -0500

    Simplify and improve documentation.

--- events/events.js
+++ events/events.js
@@ -2,71 +2,24 @@
 
 class GUIMouseFollowerComponent extends Component {
   update(ctx) {
-    //From the draw function of the engine
-    let browserAspectRatio = ctx.canvas.width / ctx.canvas.height;
-    let zeroX = 0;
-    let zeroY = 0;
-    if (EngineGlobals.requestedAspectRatio > browserAspectRatio)
-      zeroY = (ctx.canvas.height - ctx.canvas.width / EngineGlobals.requestedAspectRatio) / 2;
-    else
-      zeroX = (ctx.canvas.width - ctx.canvas.height * EngineGlobals.requestedAspectRatio) / 2;
-
-    let browserWidth = ctx.canvas.width
-    if (EngineGlobals.requestedAspectRatio <= browserAspectRatio) {
-      browserWidth -= zeroX*2
-    }
-    
-    let logicalScaling = browserWidth / EngineGlobals.logicalWidth
-
-    let x = Input.mouseX;
-    let y = Input.mouseY;
-    x -= zeroX;
-    y -= zeroY;
-    x /= logicalScaling;
-    y /= logicalScaling;
-
-
-    this.transform.x = x;
-    this.transform.y = y;
-    console.log(Input.mouseX + ", " + Input.mouseY + "-> " + x + ", " + y)
+    let screenSpace = Camera.toLogicalScreenSpace(Input.mouseX, Input.mouseY, ctx);
+    this.transform.x = screenSpace.x;
+    this.transform.y = screenSpace.y;
+    console.log(Input.mouseX + ", " + Input.mouseY + "-> " + this.transform.x + ", " + this.transform.y)
   }
 }
+
 class EventComponent extends Component {
   start() {
   }
   update(ctx) {
-
     //First adjust the camera for debugging
     Camera.main.transform.x = Math.sin(Time.time) * 10;
     Camera.main.transform.y = Math.sin(Time.time) * 10;
 
-    //From the draw function of the engine
-    let browserAspectRatio = ctx.canvas.width / ctx.canvas.height;
-    let browserWidth = ctx.canvas.width
-    if (EngineGlobals.requestedAspectRatio > browserAspectRatio) {
-
-
-    }
-    else {
-      let desiredWidth = ctx.canvas.height * EngineGlobals.requestedAspectRatio
-      let amount = (ctx.canvas.width - desiredWidth) / 2;
-      browserWidth -= 2 * amount
-    }
-
-    let logicalScaling = browserWidth / EngineGlobals.logicalWidth
-
-    let x = Input.mouseX;
-    let y = Input.mouseY;
-    x -= ctx.canvas.width / 2;
-    y -= ctx.canvas.height / 2;
-    x /= logicalScaling;
-    y /= logicalScaling;
-    x += Camera.main.transform.x;
-    y += Camera.main.transform.y;
-
-
-    this.transform.x = x;
-    this.transform.y = y;
+    let worldSpace = Camera.toWorldSpace(Input.mouseX, Input.mouseY, ctx)
+    this.transform.x = worldSpace.x;
+    this.transform.y = worldSpace.y;
     // console.log(Input.mouseX + ", " + Input.mouseY + "-> " + x + ", " + y)
   }
 }
